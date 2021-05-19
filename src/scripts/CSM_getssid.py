3244c2771d962795faa8438d4fe09d2376e4adee
Zachary Gamble <zjgamble@mymail.mines.edu>
2021-05-19 16:18:39 -0600

    Script formatting adjusted

--- src/scripts/CSM_getssid.py
+++ src/scripts/CSM_getssid.py
@@ -4,32 +4,31 @@ import subprocess
 import sys
 
 
-def get_ssid(interface:str='wlan0'):
-	"""
-	Get wireless SSID for specified interface
-	"""
-	result = subprocess.run(['iwgetid',interface,'-r'],capture_output=True)
-	if result.returncode != 0:
-		return '',False
-	output_b = result.stdout
-	output = output_b.decode('utf-8').strip()
-	return output,True
+def get_ssid(interface: str = 'wlan0'):
+    """
+    Get wireless SSID for specified interface
+    """
+    result = subprocess.run(['iwgetid', interface, '-r'], capture_output=True)
+    if result.returncode != 0:
+        return '', False
+    output_b = result.stdout
+    output = output_b.decode('utf-8').strip()
+    return output, True
 
 
 if __name__ == '__main__':
-	if len(sys.argv) > 1:
-		# take second argument to be interface name
-		ssid, status = get_ssid(sys.argv[1])
-		if status:
-			print("Interface:", sys.argv[1], "--","ESSID:",ssid)
-		else:
-			print("Interface has no SSID")
-			sys.exit(1)
-	else:
-		ssid, status = get_ssid()
-		if status:
-			print("Interface: wlan0 -- ESSID:",ssid)
-		else:
-			print("wlan0 not connected")
-			sys.exit(1)
-		
+    if len(sys.argv) > 1:
+        # take second argument to be interface name
+        ssid, status = get_ssid(sys.argv[1])
+        if status:
+            print("Interface:", sys.argv[1], "--", "ESSID:", ssid)
+        else:
+            print("Interface has no SSID")
+            sys.exit(1)
+    else:
+        ssid, status = get_ssid()
+        if status:
+            print("Interface: wlan0 -- ESSID:", ssid)
+        else:
+            print("wlan0 not connected")
+            sys.exit(1)
