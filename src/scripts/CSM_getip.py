108d643636e60ebcbf214e36bc1ab2f860f6a312
Zachary Gamble <zjgamble@mymail.mines.edu>
2021-05-19 13:44:28 -0600

    Change documentation format, update documentation, adjust script format

--- src/scripts/CSM_getip.py
+++ src/scripts/CSM_getip.py
@@ -1,41 +1,45 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
 import subprocess
 import sys
 
-# parse ifconfig output and get the ip
-def ParseOutputIp(output):
+
+def parse_output_ip(output):
+    """ parse ifconfig output and get the ip
+    """
     arr = output.split(b'\n')
     if not arr[0].find(b'RUNNING') != -1:
-        return '',False
-    ipline=arr[1].decode('utf-8').strip()
-    iparg=ipline.split(' ')[1]
-    return iparg,True
+        return '', False
+    ipline = arr[1].decode('utf-8').strip()
+    iparg = ipline.split(' ')[1]
+    return iparg, True
 
 
-# run ifconfig, check status code, and parse
-def GetInterfaceIp(int):
-    result=subprocess.run(['ifconfig', int], capture_output=True)
+def get_interface_ip(interface):
+    """  run ifconfig, check status code, and parse
+    """
+    result = subprocess.run(['ifconfig', interface], capture_output=True)
     if result.returncode != 0:
-        return '',False
-    return ParseOutputIp(result.stdout)
+        return '', False
+    return parse_output_ip(result.stdout)
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         # read command line arg
-        ip,status=GetInterfaceIp(sys.argv[1])
+        ip, status = get_interface_ip(sys.argv[1])
         if status:
-            print('Interface:',sys.argv[1],'--', 'IP:',ip)
+            print('Interface:', sys.argv[1], '--', 'IP:', ip)
         else:
             print('Interface inactive')
             sys.exit(1)
     else:
-        wlanip,wstatus=GetInterfaceIp('wlan0')
-        ethip,estatus=GetInterfaceIp('eth0')
+        wlanip, wstatus = get_interface_ip('wlan0')
+        ethip, estatus = get_interface_ip('eth0')
         if wstatus:
-            print('Interface: wlan0 -- IP:',wlanip)
+            print('Interface: wlan0 -- IP:', wlanip)
         elif estatus:
-            print('Interface: eth0 -- IP:',wlanip)
+            print('Interface: eth0 -- IP:', wlanip)
         else:
             print('No active interface')
             sys.exit(1)
