37c67ed2f8c917d801d2d7985187a88b9e212825
iradley2 <57766459+iradley2@users.noreply.github.com>
2021-05-19 12:19:09 -0600

    edited mac address detection
    allows for user input or cmd line input

---
+++ CSM_mac_detection.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import sys
+
+def getMAC():
+    input_bool = False
+    if len(sys.argv) == 1:
+        while not input_bool:
+            connection = input("What is your connection type? (ethernet, wireless)\n")
+            c_type, input_bool = determine_c_type(connection, input_bool)
+    else:
+        new_c_string = sys.argv[1]
+        c_type, input_bool = determine_c_type(new_c_string, input_bool)
+        
+    try:
+            str = open('/sys/class/net/'+c_type+'/address').read()
+    except:
+        str = "00:00:00:00:00:00"
+    return str[0:17]
+
+def determine_c_type(input_string, input_bool):
+    if input_string.lower() == 'ethernet':
+        c_type = 'eth0'
+        input_bool = True
+    elif input_string.lower() == 'wifi' or input_string.lower() == 'wireless':
+        c_type = 'wlan0'
+        input_bool = True
+    else:
+        c_type = ''
+    return c_type, input_bool
+        
+    
+    
+print(getMAC())
--- mac.py
+++
@@ -1,9 +0,0 @@
-def getMAC():
-  # Return the MAC address of the specified interface
-  try:
-    str = open('/sys/class/net/wlan0/address').read()
-  except:
-    str = "00:00:00:00:00:00"
-  return str[0:17]
-  
-print(getMAC())
