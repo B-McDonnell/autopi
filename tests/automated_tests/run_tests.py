aad6f5122b9b563fa4ae656b3654580530b2e5c5
Zachary Gamble <zjgamble@mymail.mines.edu>
2021-05-20 16:09:54 -0600

    Basic testing framework implemented

--- tests/automated_tests/run_tests.py
+++ tests/automated_tests/run_tests.py
@@ -2,5 +2,32 @@
 
 import subprocess
 import sys
+import os
+import pathlib
+from pathlib import Path
 
+def get_dirs(p: Path):
+    """
+    Get all subdirectories under a directory
+    Assumes that p is a directory
+    """
+    return [x for x in p.iterdir() if x.is_dir()]
 
+def get_tests(p: Path):
+    flist = [x for x in p.iterdir() if x.is_file()]
+    return [x for x in flist if x.suffix == '.py']
+
+abs_path = Path(sys.argv[0]).absolute().parent
+
+test_dirs = get_dirs(abs_path)
+for p in test_dirs:
+    print("===================================")
+    print("Running tests for script:", p.name)
+    print("===================================")
+    os.chdir(str(p))
+    tests = get_tests(p)
+    for test in tests:
+        print("------------------------")
+        print("Running test set:", test.stem);
+        print("------------------------")
+        subprocess.run(['python3', '-m', 'unittest', str(test.name)])
