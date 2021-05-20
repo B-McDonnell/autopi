065dadd5d1e7c8460d14b35580fa57e9c07a2f53
Zachary Gamble <zjgamble@mymail.mines.edu>
2021-05-20 17:10:09 -0600

    Testing framework implemented

--- tests/automated_tests/run_tests.py
+++ tests/automated_tests/run_tests.py
@@ -1,33 +1,70 @@
 #!/usr/bin/env python3
 
-import subprocess
 import sys
+from importlib import import_module
 import os
 import pathlib
 from pathlib import Path
+from unittest import TestLoader, TestSuite
+import unittest
+
 
 def get_dirs(p: Path):
     """
     Get all subdirectories under a directory
     Assumes that p is a directory
     """
-    return [x for x in p.iterdir() if x.is_dir()]
+    return [x for x in p.iterdir() if x.is_dir() and x.name != '__pycache__']
 
-def get_tests(p: Path):
+
+def get_test_files(p: Path):
     flist = [x for x in p.iterdir() if x.is_file()]
-    return [x for x in flist if x.suffix == '.py']
+    return [x for x in flist if x.name.endswith('_test.py')]
+
 
 abs_path = Path(sys.argv[0]).absolute().parent
 
 test_dirs = get_dirs(abs_path)
+save_dir = os.getcwd()
+runner = unittest.TextTestRunner()
+nerrors = 0
+nfailures = 0
+nskipped = 0
 for p in test_dirs:
     print("===================================")
     print("Running tests for script:", p.name)
     print("===================================")
+
+    testfiles = get_test_files(p)
+    if len(testfiles) == 0:
+        continue
+
+    os.chdir(save_dir)
+    modules = [import_module(p.name + '.' + x.stem) for x in testfiles]
     os.chdir(str(p))
-    tests = get_tests(p)
-    for test in tests:
-        print("------------------------")
-        print("Running test set:", test.stem);
-        print("------------------------")
-        subprocess.run(['python3', '-m', 'unittest', str(test.name)])
+    for i,module in enumerate(modules):
+        print("-----------------------------------")
+        print("Running tests for script:", testfiles[i].name)
+        print("-----------------------------------")
+        t = TestLoader()
+        tests = t.loadTestsFromModule(module)
+        suite = TestSuite(tests)
+        result = runner.run(suite)
+        nerrors += len(result.errors)
+        nfailures += len(result.failures)
+        nskipped += len(result.skipped)
+        print()
+    print()
+    print()
+
+print("------------------------------------------------")
+print("================================================")
+print()
+print("Tests completed")
+if nfailures > 0:
+    print(nfailures, "Failed Tests")
+if nerrors > 0:
+    print(nerrors, "Tests with Errors")
+if nskipped > 0:
+    print(nerrors, "Skipped Tests")
+print()
