1c8bfd1b00e66b06c55d90e6436c9eae9c33110b
Zachary Gamble <zjgamble@mymail.mines.edu>
2021-05-24 14:26:27 -0600

    Add is_wireless_active, fixed bugs

--- src/scripts/CSM_getssid.py
+++ src/scripts/CSM_getssid.py
@@ -4,6 +4,20 @@ import subprocess
 import sys
 
 
+def is_wireless_active(interface: str):
+    """
+    Check if the interface is an active wireless interface.
+    Parameters:
+        interface: str -- the name of an interface
+    Returns:
+        active: bool
+    Internally uses iwconfig return code to determine if the interface is wireless
+    """
+    result = subprocess.run(['iwconfig', interface], capture_output=True)
+    if result.returncode != 0:
+        return False
+    return True
+
 def get_ssid(interface: str):
     """
     Get wireless SSID for specified interface
@@ -18,16 +32,21 @@ def get_ssid(interface: str):
         return '', False
     output_b = result.stdout
     output = output_b.decode('utf-8').strip()
-    return output, True
+    return output, len(output) > 0
 
 
 if __name__ == '__main__':
     # take second argument if supplied to be interface name
     interface = sys.argv[1] if len(sys.argv) > 1 else 'wlan0'
 
-    ssid, status = get_ssid(sys.argv[1])
+    status = is_wireless_active(interface)
+    if not status:
+        print("Interface not connected, not wireless, or inactive")
+        sys.exit(1)
+
+    ssid, status = get_ssid(interface)
     if status:
-        print("Interface:", sys.argv[1], "--", "ESSID:", ssid)
+        print("Interface:", interface, "--", "ESSID:", ssid)
     else:
-        print("Interface '" + interface + "' not connected.")
+        print("Interface '" + interface + "' has no SSID (Not connected).")
         sys.exit(1)
