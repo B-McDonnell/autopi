7f6f94dbcd3a3f465a59cc7178e3e6292dffea9d
Zachary Gamble <zjgamble@mymail.mines.edu>
2021-05-25 17:45:45 -0600

    Added script to generate requests, added a few additional scripts to get information

---
+++ src/scripts/CSM_get_devid.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+
+def get_devid():
+    """
+    Returns device UUID as string
+    """
+    path = Path('/boot/CSM_device_id.txt') # TODO should this change?
+    with open(path) as f:
+        lines=f.readlines()
+    return ''.join(lines).strip()
+
+
+if __name__ == "__main__":
+    print(get_devid())
---
+++ src/scripts/CSM_get_hwid.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from pathlib import Path
+from hashlib import sha256
+
+
+def get_hw_info():
+    """
+    Returns 4 lines of cpu serial and hardware data
+    """
+    path = Path('/proc/cpuinfo')
+    with open(path) as f:
+        lines=f.readlines()
+    return lines[-4:]
+
+
+def get_hwid():
+    """
+    Returns standard hardware ID
+    
+    Implementation details:
+    Uses a sha256 checksum of the last 4 lines of /proc/cpuinfo
+    """
+    info_lines = get_hw_info()
+    raw = ''.join(info_lines).encode('utf-8')
+    return sha256(raw).hexdigest()
+
+if __name__ == "__main__":
+    print(get_hwid())
