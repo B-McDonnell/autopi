d59b3a323eb8be6836900e4fea8cbe233ad28840
iradley2 <57766459+iradley2@users.noreply.github.com>
2021-05-25 13:19:21 -0600

    Protected password on input
    import stdiomask,
    
    shows "*" after typed character

--- src/scripts/CSM_setup_home_network.py
+++ src/scripts/CSM_setup_home_network.py
@@ -3,6 +3,7 @@
 import subprocess
 import sys
 import os
+import stdiomask
 
 def set_home_network():
   
@@ -13,7 +14,7 @@ def set_home_network():
   while not password_bool:
       want_password = input("Does this network have a password? (y/n)\n")
       if want_password.lower() == 'y':
-          password = input("What is the password to this network? (It will be encrypted)\n")
+          password = stdiomask.getpass(prompt ="What is the password to this network? (It will be encrypted)\n")
           password_bool = True
           is_there_password = True
       elif want_password.lower() == 'n':
@@ -74,4 +75,3 @@ if __name__ == "__main__":
       print("Permission Denied: Must be run as super user!")
       sys.exit(1)
     set_home_network()
-
