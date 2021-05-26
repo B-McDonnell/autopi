878cda2ffc844f9587dde7cf12d769452260ad5a
iradley2 <57766459+iradley2@users.noreply.github.com>
2021-05-26 15:33:47 -0600

    fixed file to have usable functions
    can now pass in a connection type interface (wlan0, eth0) to the getMAC() function

--- src/scripts/CSM_get_mac.py
+++ src/scripts/CSM_get_mac.py
@@ -3,21 +3,23 @@ import sys
 import netifaces
 
 
-def getMAC():
-    input_bool = False
-    if len(sys.argv) == 1:
-        while not input_bool:
-            connection = input("What is your connection type? (ethernet, wireless)\n")
-            c_type, input_bool = determine_c_type(connection, input_bool)
-    else:
-        c_type = sys.argv[1]
+def getMAC(c_type):
+    # pass in c_type as 'wlan0 or eth0"
     try:
-            # str = open('/sys/class/net/'+c_type+'/address').read()
-            str = netifaces.ifaddresses(c_type)[netifaces.AF_LINK][0]['addr']
+        str = netifaces.ifaddresses(c_type)[netifaces.AF_LINK][0]['addr']
     except:
         print("Error")
         sys.exit(1)
     return str[0:17]
+
+
+def user_input():
+    input_bool = False
+    while not input_bool:
+        connection = input(
+            "What is your connection type? (ethernet, wireless)\n")
+        c_type, input_bool = determine_c_type(connection, input_bool)
+    return c_type
 
 
 def determine_c_type(input_string, input_bool):
@@ -30,5 +32,16 @@ def determine_c_type(input_string, input_bool):
     else:
         c_type = ''
     return c_type, input_bool
+
+
+def main():
+    if len(sys.argv) == 1:
+        c_type = user_input()
+    else:
+        c_type = sys.argv[1]
+    return getMAC(c_type)
+
+
 if __name__ == "__main__":
-    print(getMAC())
+    print(main())
+
