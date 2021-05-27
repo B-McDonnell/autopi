3bd1406d1573eadd23a199d8b46078b0c24cdfdf
Brendan McDonnell <brendanm149@gmail.com>
2021-05-27 16:44:23 -0600

    Updated scripts to meet coding standards

--- src/scripts/CSM_get_hw_id.py
+++ src/scripts/CSM_get_hw_id.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python3
+"""Get hardware id."""
+
 from hashlib import sha256
 
 
 def get_hw_info() -> list:
-    """
-    Returns 4 lines of cpu serial and hardware data
-    """
-    with open('/proc/cpuinfo') as f:
+    """Return 4 lines of cpu serial and hardware data."""
+    with open("/proc/cpuinfo") as f:
         lines = f.readlines()
     return lines[-4:]
 
 
 def get_hw_id() -> str:
-    """
-    Returns standard hardware ID
-    
-    Implementation details:
-    Uses a sha256 checksum of the last 4 lines of /proc/cpuinfo
-    """
+    """Return standard hardware ID hashed from the last 4 lines of /proc/cpuinfo."""
     info_lines = get_hw_info()
-    raw = ''.join(info_lines).encode('utf-8')
+    raw = "".join(info_lines).encode("utf-8")
     return sha256(raw).hexdigest()
+
 
 if __name__ == "__main__":
     print(get_hw_id())
