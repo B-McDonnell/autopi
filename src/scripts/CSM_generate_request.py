b7de00ea36bbdf9361967718a3ecffc43095776f
Zachary Gamble <zjgamble@mymail.mines.edu>
2021-05-27 11:45:08 -0600

    updated install script, modified request script

--- src/scripts/CSM_generate_request.py
+++ src/scripts/CSM_generate_request.py
@@ -6,17 +6,22 @@ import netifaces
 import json
 import ssl
 import urllib.request
+from http.client import HTTPResponse
 import subprocess
 import CSM_getip
 import CSM_get_hw_id
 import CSM_get_dev_id
 import CSM_getssid
 import CSM_get_mac
+from typing import Optional
 
 
 def get_interfaces() -> list:
     """
-    Obtain a list of available interfaces
+    Obtain a list of available interfaces.
+
+    Returns:
+        list: list of interface name strings.
     """
     ifaces = ['wlan0', 'eth0']
     ref_ifaces = netifaces.interfaces()
@@ -30,13 +35,17 @@ def get_interfaces() -> list:
 def select_interface(interfaces: list) -> (bool, str, str):
     """
     Select first interface available and get its ip address
-    Parameters:
-        interfaces: list -- list of interface strings, the first entry that has an ip will be selected
-    returns:
-         interface_available:bool, interface_name: str, ip_addr: str
-
     Note:
     To properly select the interface, you have to get its ip, so the ip is returned as well
+
+    Args:
+        interfaces (list): list of interface names, the first entry that has an ip will be selected.
+
+    returns:
+        bool: interface_available
+        str: interface_name
+        str: ip_addr
+
     """
     interface = ''
     has_ip = False
@@ -52,8 +61,11 @@ def select_interface(interfaces: list) -> (bool, str, str):
 
 def generate_shutdown_request() -> dict:
     """
-    Return field(s) needed for the request
-    In this case, 'event: shutdown'
+    Get field(s) needed for the request
+    In this case, 'event: shutdown'.
+
+    Returns:
+        dict: Dictionary of field(s)
     """
     return {'event': 'shutdown'}
 
@@ -64,12 +76,18 @@ def get_mac(interface: str) -> str:
 
 def get_service_status(service: str) -> str:
     """
-    Returns a string to be used as the service status. 
-    Obtains status from 'service sshd status' return code. 
-    0 => UP, else => DOWN
+    Returns a string to be used as the service status.
+    Obtains status from 'service sshd status' return code.
     The return codes are different per-service, but 0 should always imply UP.
+
+    Args:
+        service (str): service name.
+
+    Returns:
+        str: service status. 0 => UP, not 0 => DOWN.
     """
-    result = subprocess.run(['service', service, 'status'], capture_output=True)
+    result = subprocess.run(['service', service, 'status'],
+                            capture_output=True)
     code = result.returncode
     if code == 0:
         return 'up'
@@ -82,14 +100,17 @@ def get_ssh_status() -> str:
 
 
 def get_vnc_status() -> str:
-    # Currently, there are two services, I'm uncertain which one matters
+    # TODO Currently, there are two services, I'm uncertain which one matters
     return get_service_status('vncserver-virtuald')
-    #return get_service_status('vncserver-x11-systemd')
+    # return get_service_status('vncserver-x11-systemd')
 
 
 def generate_general_request() -> dict:
     """
-    Returns fields needed for request
+    Returns fields needed for request.
+
+    Returns:
+        dict: dictionary of field(s).
     """
     available, int_name, ip = select_interface(get_interfaces())
     if not available:  # can't send message
@@ -102,7 +123,7 @@ def generate_general_request() -> dict:
                 'mac': mac,
              }
 
-    ssid,status = CSM_getssid.get_ssid(int_name)
+    ssid, status = CSM_getssid.get_ssid(int_name)
     if status:  # ensures ssid available for interface
         fields['ssid'] = ssid
     fields['ssh'] = get_ssh_status()
@@ -114,16 +135,25 @@ def generate_general_request() -> dict:
 def get_id_fields() -> dict:
     """
     Return dev and hardware IDs
+
+    Returns:
+        dict: dictionary with fields {'hwid': hwid, 'devid': devid}
     """
-    hwid = 'abc123'  # CSM_get_hw_id.get_hw_id()
+    hwid = CSM_get_hw_id.get_hw_id()
     devid = CSM_get_dev_id.get_dev_id()
     return {'hwid': hwid, 'devid': devid}
 
 
 def load_request(request_path: Path) -> str:
     """
-    Load request JSON file as string
+    Load request JSON file as string.
     If file read fails, a blank string is returned.
+
+    Args:
+        request_path (Path): path to a file with a json request.
+
+    Returns:
+        str: file contents.
     """
     try:
         with open(request_path) as f:
@@ -131,10 +161,14 @@ def load_request(request_path: Path) -> str:
     except:
         return ''
 
+
 def save_request(request_path: Path, request: str):
     """
-    Save request JSON file
-    If file open fails, the failure is ignored as it is not critical.
+    Save request JSON file.
+    If file open/save fails, the failure is ignored as it is not critical.
+
+    Args:
+        request_path (Path): path to a file with a json request.
     """
     try:
         with open(request_path, "w") as f:
@@ -143,27 +177,35 @@ def save_request(request_path: Path, request: str):
         pass  # TODO log the event
 
 
-def send_request(request: dict):
-    API_URL = 'http://localhost:8000/'
+def send_request(api_url: str, request) -> Optional[HTTPResponse]:
+    """
+    Sends a POST request with JSON data to the specified url.
+
+    Args:
+        api_url (str): url to the target for the post request.
+        request: Anything that json.dumps(request) can convert to a JSON string.
+
+    Returns:
+        Optional[HTTPResponse]: None if request fails for any reason, otherwise, returns the response from the server.
+    """
     req_json = json.dumps(request).encode('utf-8')
-    req = urllib.request.Request(API_URL, data=req_json)
+    req = urllib.request.Request(api_url, data=req_json)
     req.add_header('Content-Type', 'application/json')
-    resp = None
-    try: 
-        resp = urllib.request.urlopen(req)  # comment for SSL
-        # ctxt = ssl.create_default_context()  # uncomment for SSL
-        # resp = request.urlopen(req, context=ctxt)  # uncomment for SSL
+    try:
+        ctxt = ssl.create_default_context()
+        resp = urllib.request.urlopen(req, context=ctxt)
+        return resp
     except(urllib.error.URLError):
-        print("Connection failed")
-        # TODO handle error better?
-    return resp  # FIXME determine return type
+        # TODO handle error better? as in, better logging
+        return None
 
 
-def parse_commandline(*args):
+def parse_commandline(*args) -> (bool, bool):
     """
     Parse the command line and return flags, just two at the moment.
     Returns:
-        shutdown_req: bool, force_req: bool
+        bool: shutdown_request. Implies force_request.
+        bool: force_request. Forces request to be sent without comparing last update.
     """
     if len(args) == 0:
         return False, False
@@ -178,11 +220,16 @@ def parse_commandline(*args):
 
 
 def main(*args):
+    """
+    Args:
+        args (list): normal commandline arguments minus script name
+    """
     CSM_ROOT = Path('/var/csm/')
-    OLD_REQ = Path(CSM_ROOT) / 'old_request.json'
+    OLD_REQ = CSM_ROOT / 'old_request.json'
+    API_URL = 'http://localhost:8000/'  # TODO retarget API URL
 
     shutdown_req, force_req = parse_commandline(*args)
-        
+
     if shutdown_req:
         request_fields = generate_shutdown_request()
     else:
@@ -203,10 +250,12 @@ def main(*args):
             request = {**request, **request_fields}
 
     print(request)
-    resp = send_request(request)
+    resp = send_request(API_URL, request)
     if resp:
         print(resp.status)
         print(resp.readlines())
+    else:
+        print("Connection failed")
 
 
 if __name__ == '__main__':
