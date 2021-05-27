5a9d0529fcd8d790e8fd38f67f9a927f89b3d6db
B McDonnell <bcm.mcdonnell@gmail.com>
2021-05-27 17:04:59 -0600

    Moved setup to a new file to allow for a more flexible entrypoint

---
+++ src/dockertools/entrypoint.sh
@@ -0,0 +1,8 @@
+
+# run setup
+./dockertools/setup.sh
+
+# run tests?
+
+# run CMD
+bash -c "$@"
--- src/dockertools/setup.sh
+++ src/dockertools/setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# if there is no file at DOCKER_HW_ID_PATH, fill it with random bytes
 if [ ! -f $DOCKER_HW_ID_PATH ]; then
 	hwid_hash=$(head --bytes 32 /dev/random | sha1sum | cut -d ' ' -f 1)
 
@@ -9,4 +10,3 @@ if [ ! -f $DOCKER_HW_ID_PATH ]; then
 	echo $hwid_hash >> $DOCKER_HW_ID_PATH
 fi
 
-exec /bin/sh -c "$1"
