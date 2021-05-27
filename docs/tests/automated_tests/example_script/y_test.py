8af6694c7dd7b29e471d04f863a12243dcc0f156
Brendan McDonnell <brendanm149@gmail.com>
2021-05-27 15:00:31 -0600

    Fixed example test scripts

--- docs/tests/automated_tests/example_script/y_test.py
+++ docs/tests/automated_tests/example_script/y_test.py
@@ -1,16 +1,24 @@
-import unittest
+"""Example test script."""
+
 import os
+import unittest
 
 
 class TestRandom(unittest.TestCase):
+    """Example unit test group."""
+
     def test_1(self):
-        self.assertEqual(os.getcwd(),"/home/pi/autopi/tests/automated_tests/script1")
+        """Example unit test."""
+        self.assertEqual(os.getcwd(), "/home/pi/autopi/tests/automated_tests/script1")
 
     def test_2(self):
+        """Example unit test."""
         self.assertTrue(True)
 
     def test_3(self):
+        """Example unit test."""
         self.fail()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
