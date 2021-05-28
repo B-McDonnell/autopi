4574011d6fe34e878cec7afd9cd6ad837028ee79
iradley2 <57766459+iradley2@users.noreply.github.com>
2021-05-28 17:13:51 -0600

    refactored, split into functions
    split into functions, improved readability

--- src/scripts/CSM_add_network_from_txt.py
+++ src/scripts/CSM_add_network_from_txt.py
@@ -2,12 +2,12 @@
 
 """Add network via .txt file."""
 
+from io import TextIOWrapper
 import subprocess
 
 # on boot, run this, then rewrite file in format...
-# sudo sh -c "printf '%s\n' '#No space after (=).' '#Priority is an int value 1,2, or 3 (3 being prioritized the most).' '#If no password/priority, leave empty.' '#This file will be reset after network is added.' '' 'ssid=' 'password=' 'priority=' > /boot/CSM_new_network.txt"
+# sudo sh -c "printf '%s\n' '#No space after (=).' '#Priority is an int value 1,2, or 3 (3 being prioritized the most).' '#If no password/priority, leave empty.' '#This file will be reset after network is added.' 'ssid=' 'password=' 'priority=' > /boot/CSM_new_network.txt"
 
-f = open("/boot/CSM_new_network.txt", "r")
 # Look for format:
 #
 # ssid=
@@ -16,28 +16,59 @@ f = open("/boot/CSM_new_network.txt", "r")
 #
 #
 
-d = {}
-for line in f:
-    if not line.lstrip().startswith("#"):
-        if len(line.strip().split("=")) > 1:
-            (key, val) = line.strip().split("=")
-            d[key] = val
 
-items = ["CSM_add_network"]
+def get_dict(f: TextIOWrapper) -> dict:
+    """Create dictionary of passed in file contents.
 
-if len(d.get("priority")) > 0:
-    items.append("--priority")
-    items.append(d.get("priority"))
+    Args:
+        f (TextIOWrapper): Text file including network info.
 
-if len(d.get("password")) > 0:
-    items.append("-p")
-    items.append(d.get("password"))
-else:
-    items.append("-n")
-items.append(d.get("ssid"))
-
-if len(d.get("ssid")) > 0:
-    subprocess.run(items)
+    Returns:
+        dict: Dictionary set up with (field, input) Ie. ('ssid', 'CSMwireless').
+    """
+    d = {}
+    for line in f:
+        if not line.lstrip().startswith("#"):
+            values = list(filter(None, line.strip().split("=")))
+            if len(values) > 1:
+                (key, val) = values
+                d[key] = val
+    return d
 
 
-f.close()
+def create_process(d: dict) -> list:
+    """Create subprocess of adding new network.
+
+    Args:
+        d (dict): Dictionary set up with (field, input) Ie. ('ssid', 'CSMwireless').
+
+    Returns:
+        list: List of arguments for subprocess to run.
+    """
+    items = ["CSM_add_network"]
+    if "priority" in d:
+        items.append("--priority")
+        items.append(d.get("priority"))
+
+    if "password" in d:
+        items.append("-p")
+        items.append(d.get("password"))
+    else:
+        items.append("-n")
+    items.append(d.get("ssid"))
+    return items
+
+
+def main():
+    """Perform main action and call helper functions.
+    """
+    f = open("/boot/CSM_new_network.txt", "r")
+    d = get_dict(f)
+    items = create_process(d)
+    if "ssid" in d:
+        subprocess.run(items)
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
