4307a6dee3e618307e99f9f32464ebc732f089a3
Brendan McDonnell <brendanm149@gmail.com>
2021-05-28 17:40:33 -0600

    Added autmoated testing to docker entrypoint

--- src/dockertools/entrypoint.sh
+++ src/dockertools/entrypoint.sh
@@ -2,7 +2,10 @@
 # run setup
 ./dockertools/setup.sh
 
-# run tests?
+# run tests
+python3 /autopi/tests/automated_tests/run_tests.py
 
 # run CMD
-bash -c "$@"
+if [ $# -ne 0 ]; then
+	bash -c "$@"
+fi
