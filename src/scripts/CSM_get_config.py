dfd247176e2aa113ae813e3e23d183eb9c99a2ef
Zachary Gamble <zjgamble@mymail.mines.edu>
2021-05-31 17:53:57 -0600

    Satisfy lint rules, add try/except block to iwconfig call, fix bug in 'API_URL=' config line parsing

--- src/scripts/CSM_get_config.py
+++ src/scripts/CSM_get_config.py
@@ -3,22 +3,32 @@
 
 import os
 
+
 # TODO REPLACE ME
 def get_api_url() -> str:
+    """Return api url as string.
+
+    This is a temporary placeholder.
+
+    Defult API_URL is 'http://localhost:8000/. If 'API_URL' is available in os.environ, then that setting is used. Otherwise, if /autopi.config exists, it will check for a 'API_URL=...' line.
+
+    Returns:
+        str: the api url.
+    """
     api_url = "http://localhost:8000/"
-    if "API_URL" in os.environ: 
+    if "API_URL" in os.environ:
         api_url = os.environ["API_URL"]
     else:
         try:
-            with open('/autopi.config') as f:
+            with open("/autopi.config") as f:
                 lines = f.readlines()
         except OSError:
             pass  # TODO what should happen here?
         else:
             for line in lines:
-                l = line.strip()
-                if line.startswith('API_URL='):
-                    api_url = line[8:].strip()
+                ln = line.strip()
+                if ln.startswith("API_URL="):
+                    api_url = ln[8:].strip()
     return api_url
 
 
