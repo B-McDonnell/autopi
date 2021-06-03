5e92450af007b3d4f66accabb082b3ade26bb716
B McDonnell <bcm.mcdonnell@gmail.com>
2021-06-03 00:44:31 -0600

    Modularized device information

--- src/scripts/device_info.py
+++ src/scripts/device_info.py
@@ -1,13 +1,28 @@
-#!/usr/bin/env python3
-"""Get hardware id."""
+"""Utilities to retrieve device information."""
 
 import os
 from hashlib import sha256
 
 
+def get_dev_id() -> str:
+    """Get device ID from boot drive.
+
+    Returns:
+        id (str): unique device id
+
+    Raises:
+        OSError: file in boot drive could not be read
+    """
+    # TODO at some point, this path might be in a config file
+    with open("/boot/CSM_device_id.txt") as f:
+        lines = f.readlines()
+    return "".join(lines)
+
+
 def get_hw_info() -> list:
     """Return 4 lines of cpu serial and hardware data."""
     info_file = "/proc/cpuinfo"
+    # TODO handle info file in config
     if "DOCKER_HW_ID_PATH" in os.environ:
         info_file = os.environ["DOCKER_HW_ID_PATH"]
     with open(info_file) as f:
@@ -20,7 +35,3 @@ def get_hw_id() -> str:
     info_lines = get_hw_info()
     raw = "".join(info_lines).encode("utf-8")
     return sha256(raw).hexdigest()
-
-
-if __name__ == "__main__":
-    print(get_hw_id())
