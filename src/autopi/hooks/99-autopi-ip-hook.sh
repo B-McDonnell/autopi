d4bb3997c8d80c4a9c0f714b7372886cbcc100d3
Brendan McDonnell <brendanm149@gmail.com>
2021-06-08 16:04:43 -0600

    Updated hooks for new script location

--- src/autopi/hooks/99-autopi-ip-hook.sh
+++ src/autopi/hooks/99-autopi-ip-hook.sh
@@ -2,6 +2,6 @@
 
 case $reason in
   BOUND | BOUND6 | CARRIER | IPV4LL | STATIC | RECONFIGURE)
-    /opt/autopi/scripts/generate_request.py net_update # TODO maybe write to a log
+    /opt/autopi/generate_request.py net_update # TODO maybe write to a log
     ;;
 esac
