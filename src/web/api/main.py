e80c18baa1e4088cca9ee2615a1a40953860906c
Zachary Gamble <zjgamble@mymail.mines.edu>
2021-06-09 07:05:37 +0000

    working basic api

--- src/web/api/main.py
+++ src/web/api/main.py
@@ -1,24 +1,18 @@
 """Test API server."""
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Cookie
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
+from typing import Optional
+
+from .core import StatusModel, UserModel
+from . import db
+
 
 app = FastAPI()
 
 
-class Status(BaseModel):
-    """Base class for status JSON."""
-
-    hwid: str
-    devid: str
-    event: str
-    ip: str
-    ssid: str
-    ssh: str
-    vnc: str
-
-
+# TODO it may simplify things to have Caddy guarantee that the user is authenticated
 @app.get("/", response_class=HTMLResponse)
 def root():
     """Serve homepage."""
@@ -50,9 +44,54 @@ def help():
     """
     return HTMLResponse(content=content, status_code=200)
 
+@app.get("/register", response_class=HTMLResponse)
+def register(username: str = Cookie(None)): # FIXME this is probably not how the username cookie is passed, if that is how shibboleth works
+    """Serve register page."""
+    if username is None:
+        raise HTTPException(status_code=401, detail="Please log in...") # TODO A redirect would probably be better
+
+    devid = db.get_unregistered_devid(username)
+
+    # FIXME Return a nicer page!
+    # TODO The contents of the page have a baked in assumption about the device file name
+    content = f"""
+    <html>
+        <head>
+            <title>Help page</title>
+        </head>
+        <body>
+            Enter the following ID in '/boot/CSM_device_id.txt'
+            <h1>{devid}</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=content, status_code=200)
+
+
+# TODO this endpoint is primarily for testing; may not be in final product
+@app.post("/api/add_user")
+def add_user(user: UserModel):
+    try:
+        db.add_user_query(user.username)
+    except Exception as e:
+        # Ensure proper error logging
+        print("Error:", e)
+    return {"response text": "It didn't crash!!"}
+
 
 @app.post("/api/status")
-def update_status(status: Status):
+def update_status(status: StatusModel):
     """Print status received."""
+    if not db.devid_exists(status.devid):
+        raise HTTPException(status_code=403) # TODO ascertain proper response to bad id; minimal information is preferable
+
+    prev_hwid = db.query_hardware_id(status.devid)
+    if prev_hwid != status.hwid:
+        db.add_raspi_warning(status.devid, "New hardware ID detected!") # TODO handle message properly
+
+    if status.event == 'shutdown':
+        db.update_status_shutdown(status)
+    else:
+        db.update_status_general(status)
     print(status)
     return {"response text": "I got the status update!", "status": status}
