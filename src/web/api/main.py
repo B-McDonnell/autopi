ad10693a4081a10a7128dae999e88224a025e6fd
Zachary Gamble <zjgamble@mymail.mines.edu>
2021-06-12 17:35:12 -0600

    Revert "Fixed import"
    This reverts commit c62546ae262b7695bbc6c06cfa9bfe8f5b3879ae.

--- src/web/api/main.py
+++ src/web/api/main.py
@@ -5,7 +5,7 @@ from fastapi.responses import HTMLResponse
 
 from .core import StatusModel, UserModel
 from .db import PiDB
-from .generate_table import Klass, Row, RowItem, build_page, construct_row
+from generate_table import Row, Klass, build_page, construct_row, RowItem
 
 app = FastAPI()
 
@@ -17,23 +17,10 @@ def root(username: str = Cookie(None)):
         warnings = db.get_user_wanings(username)
         raspis = db.get_raspis()
     warning_ids = [warning[0] for warning in warnings]
-    warning_rows = tuple(
-        Row(
-            items=(
-                RowItem("Name", warning[1], Klass.WARNING),
-                RowItem("Warning Description", warning[2], Klass.WARNING),
-            )
-        )
-        for warning in warnings
-    )
-
+    warning_rows = tuple(Row(items=(RowItem("Name", warning[1], Klass.WARNING), RowItem("Warning Description", warning[2], Klass.WARNING))) for warning in warnings)
+    
     columns = ["Name", "IP Address", "SSID", "SSH", "VNC", "Last Updated", "Power"]
-    rows = [
-        construct_row(
-            zip(columns, items[1:]), items[0], hw_warning=items[0] in warning_ids
-        )
-        for items in raspis
-    ]
+    rows = [construct_row(zip(columns, items[1:]), items[0], hw_warning=items[0] in warning_ids) for items in raspis]
 
     content = build_page(rows, warning_rows)
     return HTMLResponse(content=content, status_code=200)
