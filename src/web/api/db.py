6f21d75b0a187e698cba5da82ee031b264f23922
B McDonnell <bcm.mcdonnell@gmail.com>
2021-06-16 01:50:33 -0600

    Added autoremoval of warnings on fetch

--- src/web/api/db.py
+++ src/web/api/db.py
@@ -331,11 +331,12 @@ class PiDBConnection:
         """
         self._commit(query, (devid, warning))
 
-    def get_device_warnings(self, devid: str) -> list:
+    def get_device_warnings(self, devid: str, remove: bool = True) -> list:
         """Return list of warnings for a specific device.
 
         Args:
             devid (str): device ID.
+            remove (bool): delete warnings after retrieval
 
         Returns:
             list[(warning: str, added_at: datetime.datetime)]: the list of warnings if any
@@ -344,10 +345,20 @@ class PiDBConnection:
             SELECT warning, added_at FROM autopi.raspi_warning
             WHERE device_id=%s;
         """
-        return self._fetchall(query, (devid,))
+        remove_query = """
+            DELETE FROM autopi.raspi_warnings WHERE device_id=%s;
+        """
+        warnings = self._fetchall(query, (devid,))
+        if remove:
+            self._commit(remove_query, (devid,))
+        return warnings
 
-    def get_user_warnings(self, username: str) -> list:
+    def get_user_warnings(self, username: str, remove: bool = True) -> list:
         """Return list of warnings for a specific device.
+
+        Args:
+            username (str): username
+            remove (bool): delete warnings after retrieval
 
         Returns:
             list[(device_id: str, warning: str, added_at: datetime.datetime)]: the list of warnings if any
@@ -357,7 +368,18 @@ class PiDBConnection:
             FROM autopi.raspi_warning as w, autopi.raspi as r
             WHERE w.device_id = r.device_id AND r.username = %s;
         """
-        return self._fetchall(query, (username,))
+        remove_query = """
+            DELETE FROM autopi.raspi_warning
+            WHERE w.device_id IN (
+                SELECT device_id
+                FROM autopi.raspi
+                WHERE username = %s
+            );
+        """
+        warnings = self._fetchall(query, (username,))
+        if remove:
+            self._commit(remove_query, (username,))
+        return warnings
 
 
 @contextmanager
